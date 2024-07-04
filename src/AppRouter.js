4f6759e9c774297a73d948c8aacefc9d7b46491d
SiNiKin <tlsdl99@gmail.com>
2024-07-04 18:10:18 +0900

    ✨ Feat: Cart + Order list wireframe
    Feat: Cart + Order list wireframe

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -8,6 +8,7 @@ import ItemListView from "./items/views/ItemListView";
 import ItemDetailView from "./items/views/ItemDetailView";
 import ItemCreateView from "./items/views/ItemCreateView";
 import ItemEditView from "./items/views/ItemEditView";
+import CartsListPage from "./component/pages/CartsListPage";
 
 function AppRouter() {
     return (
@@ -21,6 +22,7 @@ function AppRouter() {
             <Route path="/items/create" element={<ItemCreateView />} />
             <Route path="/items/:id" element={<ItemDetailView />} />
             <Route path="/items/:id/edit" element={<ItemEditView />} />
+            <Route path="/cart" element={<CartsListPage />} />
         </Routes>
     );
 }

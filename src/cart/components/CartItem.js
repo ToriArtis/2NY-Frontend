00e31216c297a96c40a63973673ddca9faba496d
SiNiKin <tlsdl99@gmail.com>
2024-07-12 12:39:23 +0900

    ✨ Feat: KakaoPay Api
    Feat: KakaoPay Api

--- src/cart/components/CartItem.js
+++ src/cart/components/CartItem.js
@@ -4,6 +4,7 @@ import { getImageUrl } from "../../config/app-config";
 // 개별 장바구니 아이템 컴포넌트
 export function CartItem({ cart, onUpdateQuantity, onRemoveItem }) {
 
+    // 장바구니 수량 업데이트 핸들러
     const handleUpdateQuantity = (itemCartId, upDown) => {
         if (cart.quantity + upDown < 1) {
             alert('수량은 1개 이상만 가능합니다.');
@@ -16,15 +17,18 @@ export function CartItem({ cart, onUpdateQuantity, onRemoveItem }) {
         <div className="cart-item">
             <div className="cart-product">
                 <img src={getImageUrl(cart.thumbnail[0])} alt={cart.itemTitle} />
+                
                 <div className="product-info">
                     <h3>{cart.itemTitle}</h3>
                     <p>₩{parseInt(cart.price).toLocaleString()}</p>
+                    
                     <div className="quantity-control">
                         <button onClick={() => handleUpdateQuantity(cart.itemCartId, -1)}>-</button>
                         <span>{cart.quantity}</span>
                         <button onClick={() => handleUpdateQuantity(cart.itemCartId, 1)}>+</button>
                     </div>
                 </div>
+
             </div>
             <button className="remove-item" onClick={() => onRemoveItem(cart.itemCartId)}>삭제</button>
         </div>

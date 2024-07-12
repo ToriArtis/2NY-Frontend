f1c9395209432281a7a49f8c0370207112fbd788
unknown <gusdudco6@naver.com>
2024-07-12 12:20:04 +0900

    💄 Style: signup view style add
    style add

--- src/users/viewModels/useSignUpViewModel.js
+++ src/users/viewModels/useSignUpViewModel.js
@@ -1,26 +1,52 @@
 import { useState } from 'react';
 import { createUser, isValidEmail, isValidPassword } from '../models/User';
-import useForm from '../hooks/useForm';
 import { signup } from '../api/userApi';
 
 export function useSignUpViewModel() {
-  const [error, setError] = useState(null);
-  const { values, handleChange } = useForm({
+  const [values, setValues] = useState({
     email: '',
     password: '',
     realName: '',
     nickName: '',
-    address: '',
-    detailAddress: '',
     phone: '',
   });
 
+  const [address, setAddress] = useState({
+    postcode: '',
+    roadAddress: '',
+    jibunAddress: '',
+    detailAddress: '',
+    extraAddress: '',
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleAddressChange = (e) => {
+    const { name, value } = e.target;
+    setAddress(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleDaumPostcode = (data) => {
+    setAddress({
+      postcode: data.zonecode,
+      roadAddress: data.roadAddress,
+      jibunAddress: data.jibunAddress,
+      extraAddress: data.buildingName ? `(${data.buildingName})` : '',
+      detailAddress: '',
+    });
+  };
+
   const validateForm = () => {
-    if (!values.email || !isValidEmail(values.email)) {
+    if (!isValidEmail(values.email)) {
       setError('유효하지 않은 이메일 주소입니다.');
       return false;
     }
-    if (!values.password || !isValidPassword(values.password)) {
+    if (!isValidPassword(values.password)) {
       setError('비밀번호는 최소 8자 이상이며, 대문자, 소문자, 숫자, 특수문자를 포함해야 합니다.');
       return false;
     }
@@ -28,10 +54,15 @@ export function useSignUpViewModel() {
       setError('이름을 입력해주세요.');
       return false;
     }
+    if (!address.postcode || !address.roadAddress) {
+      setError('주소를 입력해주세요.');
+      return false;
+    }
     return true;
   };
 
   const handleSubmit = async (event) => {
+    console.log(address);
     event.preventDefault();
     setError(null);
     
@@ -43,8 +74,8 @@ export function useSignUpViewModel() {
         values.realName,
         values.password,
         values.nickName,
-        values.address,
-        values.detailAddress,
+        address.roadAddress,
+        address.detailAddress,
         values.phone
       );
       await signup(user);
@@ -55,8 +86,11 @@ export function useSignUpViewModel() {
   };
 
   return {
-    ...values,
+    values,
+    address,
     handleChange,
+    handleAddressChange,
+    handleDaumPostcode,
     handleSubmit,
     error,
   };
--- src/users/views/SignUpView.js
+++ src/users/views/SignUpView.js
@@ -1,103 +1,68 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { Container, Grid, Typography } from "@mui/material";
 import { useSignUpViewModel } from "../viewModels/useSignUpViewModel";
 import Input from "../components/common/Input";
-import { Container, Grid, Typography } from "@mui/material";
 import WhiteButton from "../../component/WhiteButton";
-import Footer from "../../component/Footer";
 import BlueButton from "../../component/BlueButton";
+import Footer from "../../component/Footer";
 
 function SignUpView() {
-    const { email, password, realName,nickName,address, detailAddress, phone, handleChange, handleSubmit, error } = useSignUpViewModel();
-  
-    return (
-        <>
-        
-        <Container component="main" maxWidth="xs" sx={{ marginTop: "8%" }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography component="h1" variant="h5" style={{ textAlign: "center" }}>
-                        <b>회원가입</b>
-                    </Typography>
-                </Grid>
-            </Grid>
-            <br />
-            <Grid >
-            {error && <p>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <Input
-                    label="이메일"
-                    type="email"
-                    name="email"
-                    value={email}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="비밀번호"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="이름"
-                    type="text"
-                    name="realName"
-                    value={realName}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="닉네임"
-                    type="text"
-                    name="nickName"
-                    value={nickName}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="주소"
-                    type="text"
-                    name="address"
-                    value={address}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="상세주소"
-                    type="text"
-                    name="detailAddress"
-                    value={detailAddress}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    label="전화번호"
-                    type="phone"
-                    name="phone"
-                    value={phone}
-                    onChange={handleChange}
-                    required
-                />
-                <div>
-                    <WhiteButton className="reset-button" type="reset" btnName={"취소"}
-                    onClick={() => window.history.back()}
-                    />
-                    <BlueButton className="signup-button" type="submit" btnName={"회원가입"}/>
-                </div>
-               
-            </form>
-            </Grid>
+  const { 
+    values, 
+    address, 
+    handleChange, 
+    handleAddressChange, 
+    handleDaumPostcode, 
+    handleSubmit, 
+    error 
+  } = useSignUpViewModel();
 
-            <Grid sx={{ marginBottom: "20%" }}>
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+    script.async = true;
+    document.body.appendChild(script);
+    return () => document.body.removeChild(script);
+  }, []);
 
-            </Grid>
-        </Container>
-        
-        <Footer />
-        </>
-    );
+  const openDaumPostcode = () => {
+    new window.daum.Postcode({
+      oncomplete: handleDaumPostcode,
+      width: 500,
+      height: 600,
+    }).open();
+  };
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs" sx={{ marginTop: "8%" }}>
+        <Typography component="h1" variant="h5" align="center">
+          <b>회원가입</b>
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
+          <Input label="이메일" type="email" name="email" value={values.email} onChange={handleChange} required />
+          <Input label="비밀번호" type="password" name="password" value={values.password} onChange={handleChange} required />
+          <Input label="이름" type="text" name="realName" value={values.realName} onChange={handleChange} required />
+          <Input label="닉네임" type="text" name="nickName" value={values.nickName} onChange={handleChange} required />
+          
+          <Input label="우편번호" type="text" name="postcode" value={address.postcode} readOnly />
+          <input type="button" onClick={openDaumPostcode} value="우편번호 찾기" />
+          <Input label="도로명주소" type="text" name="roadAddress" value={address.roadAddress} readOnly />
+          <Input label="상세주소" type="text" name="detailAddress" value={address.detailAddress} onChange={handleAddressChange} />
+          <Input label="참고항목" type="text" name="extraAddress" value={address.extraAddress} readOnly />
+          
+          <Input label="전화번호" type="tel" name="phone" value={values.phone} onChange={handleChange} required />
+          
+          <div>
+            <WhiteButton type="button" btnName="취소" onClick={() => window.history.back()} />
+            <BlueButton type="submit" btnName="회원가입" />
+          </div>
+        </form>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default SignUpView;

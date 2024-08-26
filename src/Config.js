4c4bf5387adb0681752b0d1bdda55c4f38286eb6
MJMM <57084218+Rencal3491@users.noreply.github.com>
2024-08-26 17:30:33 +0900

    Create Config.js

---
+++ src/Config.js
@@ -0,0 +1,6 @@
+export const key = 'devU01TX0FVVEgyMDI0MDcxMTExMDY1MzExNDkxMzM=';
+
+export const googleKey = '204883985169-g565j9nioanvsclt1jfr79m9fn8hgmis.apps.googleusercontent.com';
+export const naverKey = '6LS1McuAoO5zvWX1HUcD';
+export const kakaoKey = 'e7eafeef9578dfbd03a533a49a367e07';
+export const redirectUri = 'http://localhost:3000/oauth2/redirect';

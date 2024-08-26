95abac26adad84ff458d59c0f2e0e0ca9cfef251
MJMM <57084218+Rencal3491@users.noreply.github.com>
2024-08-26 18:48:55 +0900

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -3,4 +3,4 @@ export const key = 'devU01TX0FVVEgyMDI0MDcxMTExMDY1MzExNDkxMzM=';
 export const googleKey = '204883985169-g565j9nioanvsclt1jfr79m9fn8hgmis.apps.googleusercontent.com';
 export const naverKey = '6LS1McuAoO5zvWX1HUcD';
 export const kakaoKey = 'e7eafeef9578dfbd03a533a49a367e07';
-export const redirectUri = 'http://localhost:3000/oauth2/redirect';
+export const redirectUri = 'http://2ny-frontendv3-env.eba-mhpsr73c.ap-northeast-2.elasticbeanstalk.com/oauth2/redirect';
